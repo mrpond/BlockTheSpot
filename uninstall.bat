dcb6df99396b11a056445182806eb2f6de74382a
Nuzair Rasheed <38219313+rednek46@users.noreply.github.com>
2020-03-27 14:41:14 +0530

    Update uninstall.bat
    Added author name and removed an unwanted code.

--- uninstall.bat
+++ uninstall.bat
@@ -1,7 +1,7 @@
 @echo off
+echo *****************
 echo Author: @rednek46
-echo Enabling auto update for spotify...
-icacls "%localappdata%\Spotify\Update" /reset /T > NUL 2>&1
+echo *****************
 echo Removing Patch
 del /s /q "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
 move "%APPDATA%\Spotify\chrome_elf.dll.bak" "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
