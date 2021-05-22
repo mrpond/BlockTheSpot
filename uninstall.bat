c85a0fd9665ad4fd67b60704439b0bef8c0a0dc3
Daksh P. Jain <jaindaksh42@gmail.com>
2021-05-22 16:10:12 +0530

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -9,18 +9,4 @@ if exist "%APPDATA%\Spotify\chrome_elf.dll.bak" (
 ) else (
     echo done
 )
-
-if exist "%APPDATA%\Spotify\Apps\zlink.spa.bak" (
-    del /s /q "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
-    move "%APPDATA%\Spotify\Apps\zlink.spa.bak" "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
-) else (
-    echo done
-)
-
-if exist "%APPDATA%\Spotify\Apps\xpui.spa.bak" (
-    del /s /q "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
-    move "%APPDATA%\Spotify\Apps\xpui.spa.bak" "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
-) else (
-    echo done
-)
 pause
