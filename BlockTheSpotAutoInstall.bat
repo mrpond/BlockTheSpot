fc2a36794bc46382fbe0d4d618d75ecb5e258980
KUTlime <kutlime@gmail.com>
2022-02-08 14:01:31 +0100

    Remove the CMD script for auto installation

--- BlockTheSpotAutoInstall.bat
+++
@@ -1,2 +0,0 @@
-powershell -Command "Invoke-Expression ""& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/kutlime/BlockTheSpot/automatization/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify -RemoveAdPlaceholder"""
-exit
