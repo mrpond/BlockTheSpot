2f8b2813d561069566d62190e6b1ced402886c85
thororen <78185467+thororen1234@users.noreply.github.com>
2023-10-09 15:48:13 -0400

    fix link for interaction.bat

--- Spicetify&BlockTheSpot - Interaction.bat
+++ Spicetify&BlockTheSpot - Interaction.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/thororen1234/BlockTheSpot/master/install.ps1' | Invoke-Expression} -eq y"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression} -eq y"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
 pause
