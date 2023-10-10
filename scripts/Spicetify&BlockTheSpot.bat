7538564a23c8f3361b395277378478b13c4d0af8
thororen <78185467+thororen1234@users.noreply.github.com>
2023-10-09 21:38:46 -0400

    fix a possible issue in the spicetify install

--- scripts/Spicetify&BlockTheSpot.bat
+++ scripts/Spicetify&BlockTheSpot.bat
@@ -1,6 +1,7 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/scripts/install.ps1' | Invoke-Expression}"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/scripts/install.ps1' | Invoke-Expression}"
+
 pause
 exit /b
--- scripts/Spicetify&BlockTheSpotNI.bat
+++ scripts/Spicetify&BlockTheSpotNI.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/scripts/niinstall.ps1' | Invoke-Expression}"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/scripts/niinstall.ps1' | Invoke-Expression}"
 exit /b
